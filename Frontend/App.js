37086adcd358156e1714aa51463258ec7ff33555
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2023-12-30 02:42:32 +0100

    initial setup

---
+++ Frontend/App.js
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import LandingPage1 from "./Pages/LandingPage1";
+import Login from "./Pages/Login";
+export default function App() {
+  return <View style={{flex:1}}>
+    <LandingPage1/>
+  </View>;
+}
