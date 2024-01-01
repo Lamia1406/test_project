4ed602d21382b722dea0159d60076ee8c9f4f257
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-01-01 04:08:18 +0100

    Implement Home page

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,8 +3,9 @@ import LandingPage1 from "./Pages/LandingPage1";
 import Login from "./Pages/Login";
 import LandingPage2 from "./Pages/LandingPage2";
 import SignUp from "./Pages/SignUp";
+import Home from "./Pages/Home";
 export default function App() {
   return <View style={{ flex: 1 }}>
-    <Login />
+    <Home />
   </View>;
 }
