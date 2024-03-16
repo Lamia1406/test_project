6a65a5fae9a00699384c6fea532d05ae997355f4
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-03-16 16:07:16 +0100

    apply pycodestyle

--- Backend/project_conf/settings.py
+++ Backend/project_conf/settings.py
@@ -7,6 +7,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&ufv_2ad!d@nb#81k5od(^%1z9+io*1ubrckd^zm-^li3f3j&('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
